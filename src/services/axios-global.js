e69ddf524a25fc9ae1350b653615252fc35b330c
MohamedAshraf1810 <mohamedashraf3333@hotmail.com>
2025-03-20 00:23:07 +0200

    change axios service api url

--- src/services/axios-global.js
+++ src/services/axios-global.js
@@ -1,3 +1,2 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5005/";
-// axios.defaults.baseURL = "http://192.168.1.4:5005/";
+axios.defaults.baseURL = "https://resolute-typical-technician.glitch.me/";
